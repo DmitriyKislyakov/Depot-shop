b91c5f2258664c7ef6d8207ba206e62855bf62b1
Dima <ansfer91@gmail.com>
2025-01-20 00:23:00 +0300

    A2

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Product.delete_all
+
+Product.create!(title: 'programming Ruby',
+description: %(<p>Ruby is a top</p>),
+image_url: 'ruby.jpg',
+price: 49.95)
+
+Product.create!(title: 'JavaScript',
+description: %(<p> js from front</p>),
+image_url: 'js.jpg',
+price: 25.19)
